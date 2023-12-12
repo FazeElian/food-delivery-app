c93054378bd3965cc7d8deb87c6b8419403d61d3
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-12-12 17:22:06 -0500

    Creación rutas de vistas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Rutas
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/">
+        <Route path="/" /> {/* Ruta Página Principal */}
+        <Route path="products/*" /> {/* Ruta Productos */}
+        <Route path="products/categories" /> {/* Ruta Categorías de Productos */}
+        <Route path="my-favorites" /> {/* Ruta Mis Favoritos */}
+        <Route path="shopping-cart" /> {/* Ruta Carrito de Compras */}
+        <Route path="explore" /> {/* Ruta Explorar */}
+        <Route path="notifications" /> {/* Ruta Notificaciones */}
+        <Route path="profile" /> {/* Ruta Perfil */}
+      </Route>
+      
+      {/* Ruta Error 404 - Página no encontrada */}
+      {/* <Route path="*" element={<Error404View />} /> */}
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Rutas con Hash
+import { HashRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
