934bc3be93d6648fb8b7945029a7852ed68def41
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-12-19 16:07:56 -0500

    Implementando Encabezado de App a través de las rutas a todas las vistas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 // Componentes de vistas
+  // Encabezado de página
+  import { Header } from "./components/Header";
+
   // Página Principal 
   import { HomeView } from "./views/HomeView";
 
@@ -14,16 +17,18 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeView />} /> {/* Ruta Página Principal */}
-        <Route path="products/*" /> {/* Ruta Productos */}
-        <Route path="products/categories" /> {/* Ruta Categorías de Productos */}
-        <Route path="my-favorites" /> {/* Ruta Mis Favoritos */}
-        <Route path="shopping-cart" /> {/* Ruta Carrito de Compras */}
-        <Route path="explore" /> {/* Ruta Explorar */}
-        <Route path="notifications" /> {/* Ruta Notificaciones */}
-        <Route path="user/*" >
-          <Route path="configuration" />
-          <Route path="profile" />
+        <Route path="/" element={<Header />} > {/* Ruta Página Principal */}
+          <Route path="/" index element={<HomeView />} />
+          <Route path="products/*" /> {/* Ruta Productos */}
+          <Route path="products/categories" /> {/* Ruta Categorías de Productos */}
+          <Route path="my-favorites" /> {/* Ruta Mis Favoritos */}
+          <Route path="shopping-cart" /> {/* Ruta Carrito de Compras */}
+          <Route path="explore" /> {/* Ruta Explorar */}
+          <Route path="notifications" /> {/* Ruta Notificaciones */}
+          <Route path="user/*" >
+            <Route path="configuration" />
+            <Route path="profile" />
+          </Route>
         </Route>
         
         {/* Ruta Error 404 - Página no encontrada */}
