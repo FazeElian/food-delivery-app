769dc697b3991686a75275bd99feae98e14dfc53
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-12-19 11:58:30 -0500

    Responsive de Banner de Hamburguesa

--- src/components/modules/Home/BannerBurger.jsx
+++ src/components/modules/Home/BannerBurger.jsx
@@ -13,42 +13,44 @@ import React, { Suspense } from "react";
 import { BurgerModel } from "./BurgerModel";
 
 import CocaColaImage from "../../../assets/img/modules/Home/coca-cola.png";
-import FrenchFriesImage from "../../../assets/img/modules/Home/french-fries.png";
+import FrenchFriesImage from "../../../assets/img/modules/Home/french-fries-mc-donals.png";
 
 const BannerBurger = () => {
     return(
         <>
             <nav className="cont-banner-burger">
-                <div className="cont-burger">
-                    <Canvas camera={{zoom: 3, position:[20, 12, 20]}} className="banner-burger-model animate__animated animate__pulse animate__repeat-2">
-                        <ambientLight intensity={0.75} /> 
-                        <Suspense fallback={null} />
-                        <OrbitControls enableZoom={false} /> 
+                <div className="container-burger-model--info">
+                    <div className="cont-burger" style={{ position: 'relative', zIndex: 1 }}>
+                        <Canvas camera={{zoom: 3, position:[20, 12, 20]}} className="banner-burger-model">
+                            <ambientLight intensity={0.75} /> 
+                            <Suspense fallback={null} />
+                            <OrbitControls enableZoom={false} /> 
 
-                        <pointLight position={[2, 15, 2]} intensity={100} />
-                        <pointLight position={[1, 0.5, 1]} intensity={100} />
-                        <pointLight position={[1, 1, 1]} intensity={400} />
-                        <pointLight position={[1, 1, 1]} intensity={400} />
-                        <pointLight position={[10, 0.5, 10]} intensity={380} />
+                            <pointLight position={[2, 15, 2]} intensity={100} />
+                            <pointLight position={[1, 0.5, 1]} intensity={100} />
+                            <pointLight position={[1, 1, 1]} intensity={400} />
+                            <pointLight position={[1, 1, 1]} intensity={400} />
+                            <pointLight position={[10, 0.5, 10]} intensity={380} />
 
-                        <BurgerModel />
-                    </Canvas>
-                </div>
-                <div className="cont-content-burger">
-                    <h2>Hamburguesa Express</h2>
-                    <div className="content-burger-price">
-                        <h3>Sólo por </h3>
-                        <h1>4.500 $</h1>
+                            <BurgerModel />
+                        </Canvas>
+                    </div>
+                    <div className="cont-info-burger">
+                        <h2>Hamburguesa Express</h2>
+                        <div className="content-burger-price">
+                            <h3>Sólo por </h3>
+                            <h1>4.500 $</h1>
+                        </div>
                     </div>
                 </div>
-                <div className="cont-content-burger cont-include-burger">
+                <div className="cont-include-burger">
                     <div className="content-cont-include-burger">
                         <div className="cont-includes-burger">
                             <h2>Incluye: </h2>
                             <div className="cont-elements-include">
                                 <div className="cont-element-include">
                                     <img src={CocaColaImage} alt="" />
-                                    <p>Coca Cola personal 400 ml</p>
+                                    <p>Coca Cola en lata 200 ml</p>
                                 </div>
                                 <div className="cont-element-include cont-include-paragraph-btm">
                                     <img src={FrenchFriesImage} alt="" />
