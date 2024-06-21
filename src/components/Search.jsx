f9b78bfe161c349cb4bdd2dd63a6a628d224bbab
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-06-21 13:37:34 -0500

    Responsive Barra de Búsqueda

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,23 +5,29 @@ import "../assets/css/components/Search.css";
     // Search
     import SearchIcon from "../assets/img/icons/search.png";
 
+    // Filter
+    import FilterIcon from "../assets/img/icons/filter.png";
+
 const Search = () => {
     return(
         <div className="cont-search">
             <div className="cont-order-by-search">
                 <h2>Ordenar Por</h2>
                 <select name="order-prods-by" id="">
-                    <option value="">Más relevantes</option>
+                    <option value="">Más Relevantes</option>
                     <option value="">Precio (Mayor a menor) </option>
                     <option value="">Precio (Menor a mayor) </option>
-                    <option value="">A - Z</option>
+                    <option value="">Orden Alfabético (A - Z)</option>
                 </select>
+                <button className="btn-filter-prods">
+                    <img src={FilterIcon} alt="" />
+                </button>
             </div>
             <div className="cont-search-bar-search">
                 <div className="cont-search-icon">
                     <img src={SearchIcon} alt="Search Icon" />
                 </div>
-                <input type="search" className="search-bar" placeholder="Buscar Producto..." />
+                <input type="text" className="search-bar" placeholder="Buscar Producto" />
             </div>
         </div>
     )
